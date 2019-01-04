d9fcc4ef7a51e6559991123fa4b90b649de9fdf0
Robin Wilson <robinwilson@rwsbetas.com>
2019-01-04 00:23:25 +0000

    Removed sample images now stock refreshes. Updated SSIS package

--- MainlineUK/Pages/SellYourCar.cshtml.cs
+++ MainlineUK/Pages/SellYourCar.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +14,20 @@ namespace MainlineUK.Pages
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            SmtpClient client = new SmtpClient("rwshosting.com");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("enquiries@mainlineuk.co.uk", "Sonyvaio1");
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("enquiries@mainlineuk.co.uk");
+            mailMessage.To.Add("enquiries@mainlineuk.co.uk");
+            mailMessage.Body = "body123";
+            mailMessage.Subject = "subject123";
+            client.Send(mailMessage);
         }
     }
 }
